5c564f213c424adc825b60101c014de8e0324ae8
Adrian Nicolai Lumbad <anlumbad@gmail.com>
2021-07-26 15:48:27 +0800

    refractored students controller and student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,21 +5,40 @@ class Student < ApplicationRecord
   	validates :first_name, :last_name, presence: true
 
 	# returns the the student with id equal to the given id 
-	def self.find_student_by_id(id)
-		return Student.find(id)
+	def self.find_student_by_id(student_id)
+		ActiveRecord::Base.connection.select_one(
+			ActiveRecord::Base.send(:sanitize_sql_array,
+			  ["SELECT * FROM students 
+				WHERE id = ?;",
+				student_id]
+			)
+		)
 	end
 
 	# creates a new studennt
-	def self.create_student(student_params, dojo_id)
-		student 	   = Student.new(student_params)
-		student.dojo   = Dojo.find_dojo_by_id(dojo_id) 
-		student.save
+	def self.create_student(params, dojo_id)
+		date_time_now = Time.now.utc.strftime("%Y-%m-%d %H:%M:%S")
+		ActiveRecord::Base.connection.insert(
+			ActiveRecord::Base.send(:sanitize_sql_array,
+			  ["INSERT INTO students (first_name, last_name, email, dojo_id, created_at, updated_at)
+				VALUES(?, ?, ?, ?, '#{date_time_now}', '#{date_time_now}');",
+				params["first_name"], params["last_name"], params["email"], dojo_id]
+			)
+		)
 	end
 
 	# updates the selected student with the given parameters
-	def self.update_student(student_id, student_params, dojo_id)
-		dojo = Dojo.find_dojo_by_id(dojo_id)
-		Student.find(student_id).update(student_params)
-		Student.find(student_id).update(dojo: dojo)
+	def self.update_student(student_id, params, dojo_id)
+		ActiveRecord::Base.connection.update(
+			ActiveRecord::Base.send(:sanitize_sql_array,
+			  ["UPDATE students
+				SET first_name = ?, last_name = ?, email = ?, dojo_id = ?
+				WHERE id = ?;",
+				params["first_name"], params["last_name"], params["email"], dojo_id, student_id]
+			)
+		)
+		# dojo = Dojo.find_dojo_by_id(dojo_id)
+		# Student.find(student_id).update(student_params)
+		# Student.find(student_id).update(dojo: dojo)
 	end
 end
