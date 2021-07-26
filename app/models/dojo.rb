7aea91bedb9a43437b6421c9ddd4acbe3bc3a10a
Adrian Nicolai Lumbad <anlumbad@gmail.com>
2021-07-26 17:26:40 +0800

    added comments for controllers and models

--- app/models/dojo.rb
+++ app/models/dojo.rb
@@ -2,15 +2,23 @@ class Dojo < ApplicationRecord
     has_many :students
     validates :branch, :street, :city, :state, presence: true
 
-    # Select all records from dojos table
-    # to return all records in dojos table
+    # DOCU: Gets all records in the dojos table 
+    # Triggered by: dojos_controller > index, students_controller > edit, students_controller > new
+    # Returns: All records from dojos table, false
+    # Owner: Adrian
     def self.all_dojos
         return ActiveRecord::Base.connection.exec_query(
                                 "SELECT * FROM dojos"
-                                )
+							)
+	rescue Exception
+		  return  false
     end
 
-    # Create a new Dojo
+	# DOCU: Create a new Dojo
+    # Triggered by: dojos_controller > create
+    # Requires: branch, street, city, state
+    # Returns: true, false
+    # Owner: Adrian
     def self.create_new_dojo params
 		date_time_now = Time.now.utc.strftime("%Y-%m-%d %H:%M:%S")
 		ActiveRecord::Base.connection.execute(
@@ -20,10 +28,16 @@ class Dojo < ApplicationRecord
 				params["branch"], params["street"], params["city"], params["state"]]
 			)
 		)
-		return { :status => true }
+		return true
+	rescue Exception
+		  return  false
     end
 
-    # returns the the dojo with id equal to the given id 
+    # DOCU: Finds the dojo with the corresponding dojo id
+    # Triggered by:  dojos_controller > show, dojos_controller > edit, dojos_controller > update, dojos_controller > destroy
+    # Requires: dojo_id
+    # Returns: selected record, false
+    # Owner: Adrian
     def self.find_dojo_by_id(dojo_id)
         ActiveRecord::Base.connection.select_one(
             ActiveRecord::Base.send(:sanitize_sql_array,
@@ -32,20 +46,30 @@ class Dojo < ApplicationRecord
                 dojo_id])
         )
     rescue Exception
-		return  false
+		  return  false
     end
 
+	# DOCU: Finds the students with the corresponding dojo_id
+    # Triggered by:  dojos_controller > show
+    # Requires: dojo_id
+    # Returns: selected records, false
+    # Owner: Adrian
     def self.find_students_by_dojo_id(dojo_id)
         ActiveRecord::Base.connection.execute(
             ActiveRecord::Base.send(:sanitize_sql_array,
               ["SELECT * FROM students 
                 WHERE dojo_id = ?;",
-                dojo_id])
+                dojo_id]
+              )
         )
     rescue Exception
 		  return  false
     end
 
+	# DOCU: Finds the dojo with the corresponding dojo id
+    # Triggered by:  dojos_controller > show, dojos_controller > edit, dojos_controller > update, dojos_controller > destroy
+    # Requires: dojo_id
+    # Owner: Adrian
 	def self.delete_dojo_by_id(dojo_id)
 		ActiveRecord::Base.connection.delete(
 			ActiveRecord::Base.send(:sanitize_sql_array,
@@ -57,6 +81,10 @@ class Dojo < ApplicationRecord
 		return  false
 	end
 
+	# DOCU: Updates the record for the selected dojo
+    # Triggered by:  dojos_controller > update
+    # Requires: dojo_id
+    # Owner: Adrian
 	def self.update_dojo(dojo_id, params)
 		ActiveRecord::Base.connection.update(
 			ActiveRecord::Base.send(:sanitize_sql_array,
