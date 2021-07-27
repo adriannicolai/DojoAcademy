34002c735956f249927ce2978def03392dbd2a1b
Adrian Nicolai Lumbad <anlumbad@gmail.com>
2021-07-27 16:30:55 +0800

    added feature for edit ajax

--- app/assets/javascript/dojos.js
+++ app/assets/javascript/dojos.js
@@ -24,16 +24,33 @@ $(document).ready(function() {
         .on("click", "a", function(e){
             if($(this).text() == "Edit"){
                 e.preventDefault()
-
                 $.get($(this).attr("href"), function (res) {
-                    $("#editDojoForm").html(res)
+                    $("#editDojoModalTitle").text("Editing " + res.branch)
+                    $("#updateDojoForm").attr("action", "/dojos/"+ res.id)
+                    $("#editDojoModalBranch").val(res.branch)
+                    $("#editDojoModalStreet").val(res.street)
+                    $("#editDojoModalCity").val(res.city)
+                    $("#editDojoModalState").val(res.state)
                 })
                 $("#editDojoModal").modal("show")
             }
-            else{
-
-            }
         })
-        
+        .on("submit", "#updateDojoForm", function(e){
+            e.preventDefault()
+            $.post($(this).attr("action"), $(this).serialize(), function(res){
+                console.log(res)
+                let html  = "<td>" + res.branch +"</td>"
+                html     += "<td>" + res.street + "</td>"
+                html     += "<td>" + res.city + "</td>"
+                html     += "<td>" + res.state + "</td>"
+                html     += "<td>"
+                html     += "<a href='/dojos/"+ res.id +"'>Show</a> "
+                html     += "<a href='/dojos/"+ res.id +"/edit'>Edit</a> "
+                html     += "<a href='/dojos/"+ res.id +"/destroy'>Destroy</a>"
+                html     += "</td>"
+                $("#dojo"+ res.id).html(html)
+            })
+            $("#editDojoModal").modal("hide")
+        })
 
 })
