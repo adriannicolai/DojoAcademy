6c601e33ab439e9f9ab5334bfd0c683956e181a6
Adrian Nicolai Lumbad <anlumbad@gmail.com>
2021-07-27 14:33:24 +0800

    adding features

--- app/assets/javascript/dojos.js
+++ app/assets/javascript/dojos.js
@@ -1,21 +1,39 @@
 $(document).ready(function() {
-    $(document).on('click', '#newDojo', function(){
-        $('#newDojoModal').modal("show")
-    })
-    $(document).on("click", ".close-modal", function(){
-        $('#newDojoModal').modal("hide")
-    })
-    $(document).on("submit", "#newDojoForm", function(e){
-        e.preventDefault()
-        $('#newDojoModal').modal("hide")
-        $.post($(this).attr('action'), $(this).serialize(), function(){
-            let html = "<tr>"
-            html    += "<td>"+ $("#input_branch") +"</td>"
-            html    += "<td>" + $("#input_street") + "</td>"
-            html    += "<td>" + $("#input_city") + "</td>"
-            html    += "<td>" + $("#input_state") + "</td>"
-            html    += "</tr>" 
-            $("tbody").append(html)
+    $(document)
+        .on('click', '#newDojo', function(){
+            $('#newDojoModal').modal("show")
         })
-    })
+        .on("click", ".close-modal", function(){
+            $('#newDojoModal').modal("hide")
+            $('#editDojoModal').modal("hide")
+        })
+        .on("submit", "#newDojoForm", function(e){
+            e.preventDefault()
+            $('#newDojoModal').modal("hide")
+            $.post($(this).attr('action'), $(this).serialize(), function(){
+                let html = "<tr>"
+                html    += "<td>"+ $("#input_branch") +"</td>"
+                html    += "<td>" + $("#input_street") + "</td>"
+                html    += "<td>" + $("#input_city") + "</td>"
+                html    += "<td>" + $("#input_state") + "</td>"
+                html    += "</tr>" 
+                $("tbody").append(html)
+            })
+            return false
+        })
+        .on("click", "a", function(e){
+            if($(this).text() == "Edit"){
+                e.preventDefault()
+
+                $.get($(this).attr("href"), function (res) {
+                    $("#editDojoForm").html(res)
+                })
+                $("#editDojoModal").modal("show")
+            }
+            else{
+
+            }
+        })
+        
+
 })
